f97b2a269f2e32c037da4e71166522c00de3ab3a
Matthew Webb <ja3g3r1@gmail.com>
2017-12-08 13:49:14 -0600

    added login draft -mw

--- EventsSchedulerUI/EventsSchedulerUI/Form1.Designer.cs
+++ EventsSchedulerUI/EventsSchedulerUI/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace EventsSchedulerUI
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         /// Required designer variable.
@@ -28,59 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.lvlSubHeader = new System.Windows.Forms.Label();
+            this.btnAdmin = new System.Windows.Forms.Button();
+            this.btnUser = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // lblHeader
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(126, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.lblHeader.AutoSize = true;
+            this.lblHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeader.Location = new System.Drawing.Point(12, 9);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(374, 25);
+            this.lblHeader.TabIndex = 0;
+            this.lblHeader.Text = "Welcome to the Events Scheduler.";
             // 
-            // label2
+            // lvlSubHeader
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(125, 50);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.lvlSubHeader.AutoSize = true;
+            this.lvlSubHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvlSubHeader.Location = new System.Drawing.Point(70, 34);
+            this.lvlSubHeader.Name = "lvlSubHeader";
+            this.lvlSubHeader.Size = new System.Drawing.Size(259, 24);
+            this.lvlSubHeader.TabIndex = 1;
+            this.lvlSubHeader.Text = "Please select a sign-in option.";
             // 
-            // button1
+            // btnAdmin
             // 
-            this.button1.Location = new System.Drawing.Point(0, 119);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnAdmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdmin.Location = new System.Drawing.Point(17, 73);
+            this.btnAdmin.Name = "btnAdmin";
+            this.btnAdmin.Size = new System.Drawing.Size(174, 54);
+            this.btnAdmin.TabIndex = 2;
+            this.btnAdmin.Text = "Event Administrator";
+            this.btnAdmin.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnUser
             // 
-            this.button2.Location = new System.Drawing.Point(201, 119);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUser.Location = new System.Drawing.Point(212, 73);
+            this.btnUser.Name = "btnUser";
+            this.btnUser.Size = new System.Drawing.Size(174, 54);
+            this.btnUser.TabIndex = 3;
+            this.btnUser.Text = "Participant";
+            this.btnUser.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(404, 153);
+            this.Controls.Add(this.btnUser);
+            this.Controls.Add(this.btnAdmin);
+            this.Controls.Add(this.lvlSubHeader);
+            this.Controls.Add(this.lblHeader);
+            this.Name = "Login";
+            this.Text = "Login";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -88,10 +92,10 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.Label lvlSubHeader;
+        private System.Windows.Forms.Button btnAdmin;
+        private System.Windows.Forms.Button btnUser;
     }
 }
 
