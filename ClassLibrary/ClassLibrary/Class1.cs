5f5265af76e44dbb1ff7dee34e33f4c891e1773d
Cailynn White <Cailynn888@gmail.com>
2017-12-10 09:24:38 -0600

    added SQL using statments to all classes

--- ClassLibrary/ClassLibrary/Class1.cs
+++ ClassLibrary/ClassLibrary/Class1.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Data.SqlClient;
+using System.Data.Sql;
+
 namespace ClassLibrary
 {
     public class Class1
--- ClassLibrary/ClassLibrary/Participant.cs
+++ ClassLibrary/ClassLibrary/Participant.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using System.Data.SqlClient;
+using System.Data.Sql;
+
 namespace ClassLibrary
 {
     public class Participant : UserType
