76080504e9c0025bfa62ab0b050a2d283cc420f0
programmer <programmer@BB21502>
2017-12-13 09:58:51 -0600

    Added ReadEventData to the Event class

--- ClassLibrary/ClassLibrary/EventInfo.cs
+++ ClassLibrary/ClassLibrary/EventInfo.cs
@@ -15,5 +15,28 @@ namespace ClassLibrary
         public string Location { get; set; }
 
         public string Title { get; set; }
+
+        // Set all data from the Event database into the properties
+
+        public void ReadEventData()
+        {
+            // Create a working connection to the database.
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = "Server=cis1.actx.edu;Database=project2;User Id=db2;Password = db20;";
+            connection.Open();
+
+            // Create a sql command
+            SqlCommand readParticipant = connection.CreateCommand();
+            readParticipant.CommandText = "select * from Event";
+            SqlDataReader reader = readParticipant.ExecuteReader();
+
+            // execute the reader
+            while (reader.Read())
+            {
+                Title = (string)reader["EventName"];
+                Location = (string)reader["Location"];
+                Description = (string)reader["Description"];
+            }
+        }
     }
 }
